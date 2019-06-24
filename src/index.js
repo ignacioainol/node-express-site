2e694d9aee8aa8f620779ee69926c9bd60e0fa1a
Ignacio <shoik.xtazy@gmail.com>
2019-06-24 00:12:21 -0400

    separated head & nav

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 //settings
 app.set('port',3000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
 
 //middlewares
 
 //routes
-app.get('/',(req,res) => {
-	res.sendFile(path.join(__dirname,'views/index.html'));
-	//console.log(path.join(__dirname,'views/index.html'));
-});
+app.use(require('./routes/index'));
 
 //static files
 
